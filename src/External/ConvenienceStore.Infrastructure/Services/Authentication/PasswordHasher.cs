c103f094ebf8f8332ff901eae5f76c93997f63b7
Lain <chiyomiziwari@gmail.com>
2026-07-25 11:22:17 +0700

    Add JWT authentication and password hashing services
    Implemented JWT-based authentication and password hashing using BCrypt. Added `IJwtProvider` and `IPasswordHasher` interfaces, their implementations, and a `JwtSettings` configuration class. Updated dependency injection to register these services and configure JWT authentication from app settings. Removed obsolete commented code.

---
+++ src/External/ConvenienceStore.Infrastructure/Services/Authentication/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using ConvenienceStore.Application.Services.Authentication;
+
+namespace ConvenienceStore.Infrastructure.Services.Authentication
+{
+    internal class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
