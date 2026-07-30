266f22f103bcd341dad43876ad0e0e341f829690
Lain <chiyomiziwari@gmail.com>
2026-07-30 21:57:12 +0700

    Add EmailChangeRequest repository interface and implementation
    Introduced IEmailChangeRequestReqpository for EmailChangeRequest entities in the domain layer. Added EmailChangeRequestRepository implementation in the persistence layer, inheriting from the generic Repository base class. Updated using directives and namespaces for consistency.

---
+++ src/External/ConvenienceStore.Persistence/Repositories/Identity/EmailChangeRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using ConvenienceStore.Domain.Entities.Identity;
+using ConvenienceStore.Domain.Repositories.Identity;
+using ConvenienceStore.Persistence.Context;
+
+namespace ConvenienceStore.Persistence.Repositories.Identity
+{
+    internal class EmailChangeRequestRepository(ConvenienceStoreDbContext context)
+        : Repository<EmailChangeRequest>(context), IEmailChangeRequestReqpository
+    {
+    }
+}
